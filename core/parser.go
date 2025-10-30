29f8bf48ddd8f953e02702692d4a60c44b58ee18
wangduanduan <eddie2072@icloud.com>
2025-10-30 17:42:09 +0800

    wip

--- core/parser.go
+++ core/parser.go
@@ -1,5 +1,20 @@
 package core
 
+type Message struct {
+	Method  string
+	Version string
+	URI     string
+	Status  int // 0 for request, >0 for reply
+	Reason  string
+
+	CallID string
+
+	Headers []string
+
+	Body []byte
+	raw  []byte
+}
+
 func base_parser(buf []byte) (int, error) {
 	// Implement your parsing logic here
 	return len(buf), nil
